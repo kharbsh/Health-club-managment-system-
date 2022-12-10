5ecc4d741649cfa8840c75109d0e2d72cbacb076
OmarAlhaj10 <119157520+OmarAlhaj10@users.noreply.github.com>
2022-12-10 14:48:54 +0200

    Add files via upload

---
+++ Connection_Class.java
@@ -0,0 +1,23 @@
+package Gym_manegement;
+
+import java.sql.*;
+
+public class Connection_Class {
+    /*
+    Connection con;
+    Statement stm;
+    Connection_Class(){
+    try{
+    Class.forName("");
+    con=DriveManeger.getConnection("");
+    stm=con.createStatement();
+    }
+    catch(exception e){
+    e.printStackTrace();
+    }
+    }
+    public static void main(String s[]){
+    new ConnectionClass();
+    }
+    */
+}
